2e704c4779f31fb1923416feecbaf5823dd06ef3
syedishmam <syedishmam17@gmail.com>
2019-12-15 18:05:46 -0800

    Start Digital Display comp

--- react_calc/src/components/App.js
+++ react_calc/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import DigitalDisplay from './DigitalDisplay';
+import '../styles/App.css';
+
 class App extends React.Component {
     render() {
         return(
-            <div>
-                <h1>Hello from App!</h1>
+            <div className="container">
+                <DigitalDisplay />
             </div>
         )
     }
---
+++ react_calc/src/components/DigitalDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DigitalDisplay extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Digital Display</h1>
+            </div>
+        )
+    }
+}
+
+export default DigitalDisplay;
