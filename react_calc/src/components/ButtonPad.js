78e2a469246478cc8ebbe7e922c7e9910f489850
syedishmam <syedishmam17@gmail.com>
2019-12-15 18:31:12 -0800

    Add all calculator buttons

--- react_calc/src/components/ButtonPad.js
+++ react_calc/src/components/ButtonPad.js
@@ -10,9 +10,34 @@ class ButtonPad extends React.Component {
                 <table>
                     <tbody>
                         <tr>
+                            <th><Button number="C"/></th>
+                            <th><Button number="+/-"/></th>
+                            <th><Button number="%"/></th>
+                            <th><Button number="/"/></th>
+                        </tr>
+                        <tr>
+                            <th><Button number="7"/></th>
+                            <th><Button number="8"/></th>
+                            <th><Button number="9"/></th>
+                            <th><Button number="X"/></th>
+                        </tr>
+                        <tr>
+                            <th><Button number="4"/></th>
+                            <th><Button number="5"/></th>
+                            <th><Button number="6"/></th>
+                            <th><Button number="-"/></th>
+                        </tr>
+                        <tr>
                             <th><Button number="1"/></th>
                             <th><Button number="2"/></th>
                             <th><Button number="3"/></th>
+                            <th><Button number="+"/></th>
+                        </tr>
+                        <tr>
+                            <th><Button number="0"/></th>
+                            <th><Button number="0"/></th>
+                            <th><Button number="."/></th>
+                            <th><Button number="="/></th>
                         </tr>
                     </tbody>
                 </table>
