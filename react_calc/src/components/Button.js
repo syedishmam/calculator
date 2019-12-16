2ba72cb46ee2946cc83187e4d0570db89430ae9d
syedishmam <syedishmam17@gmail.com>
2019-12-15 19:15:09 -0800

    Add properties if button is zero

--- react_calc/src/components/Button.js
+++ react_calc/src/components/Button.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import '../styles/Button.css';
+
 class Button extends React.Component {
+
+    ifZeroButton() {
+        if(this.props.number === "0") {
+            return <button className="button zeroButton">{this.props.number}</button>
+        } else {
+            return <button className="button">{this.props.number}</button>
+        }
+    }
+
     render() {
         return(
             <div>
-                <button>{this.props.number}</button>
+                {this.ifZeroButton()}
             </div>
         )
     }
