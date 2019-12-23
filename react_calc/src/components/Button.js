e758b1d3b427102db9e7bcc140bf89d3a2949bfc
syedishmam <syedishmam17@gmail.com>
2019-12-22 17:36:26 -0800

    Add operator. clear. backspace logic

--- react_calc/src/components/Button.js
+++ react_calc/src/components/Button.js
@@ -6,27 +6,84 @@ import '../styles/Button.css';
 
 class Button extends React.Component {
 
-    ifZeroButton() {
-        if(this.props.number === "0") {
-            return <button className="button zeroButton" onClick={() => this.clickButton(this.props.number)}>{this.props.number}</button>
-        } else {
-            return <button className="button" onClick={() => this.clickButton(this.props.number)}>{this.props.number}</button>
+    buttonType() {
+        switch(this.props.unit) {
+            case '0':
+                return <button className="button zeroButton" onClick={() => this.clickButton(this.props.unit)}>{this.props.unit}</button>
+
+            default:
+                return <button className="button" onClick={() => this.clickButton(this.props.unit)}>{this.props.unit}</button>
         }
     }
 
-    clickButton(number) {
-        if(this.props.keysPressed) {
-            let keys = this.props.keysPressed.concat(number);
-            this.props.keyPress(keys);
-        } else {
-            this.props.keyPress(number);
+    clickButton(unit) {
+        const allKeys = this.props.keysPressed;
+        if(unit === 'C') {
+            this.props.keyPress('1');
+        } else if(unit === 'DEL') {
+            this.deleteButtonLogic(allKeys);
+        } else if(unit === '+/-') {
+            this.negateButtonLogic(allKeys)
+        } else if(unit === '/') {
+            this.operatorButtonLogic(allKeys, '/');
+        } else if(unit === '*') {
+            this.operatorButtonLogic(allKeys, '*');
+        } else if(unit === '-') {
+            this.operatorButtonLogic(allKeys, '-');
+        } else if(unit === '+') {
+            this.operatorButtonLogic(allKeys, '+');
+        }
+    }
+
+    deleteButtonLogic(keys) {
+        if(keys) {
+            const deleted = keys.substring(0, keys.length - 1);
+            return this.props.keyPress(deleted);
+        }
+    }
+
+    negateButtonLogic(keys) {
+        if(keys) {
+            if(keys[0] !== '-' && keys !== 0) {
+                const negateKeys = '-'.concat(keys);
+                this.props.keyPress(negateKeys);
+            } else {
+                const removeNegative = keys.substring(1, keys.length);
+                this.props.keyPress(removeNegative);
+            }
+        }
+    }
+
+    operatorButtonLogic(keys, operator) {
+        let pushOperator;
+        if(keys && keys[keys.length - 1] !== '/' && keys[keys.length - 1] !== '*' && keys[keys.length - 1] !== '-' && keys[keys.length - 1] !== '+') {
+            if(operator === '/') {
+                pushOperator = keys.concat('/'); 
+                this.props.keyPress(pushOperator);
+            } else if(operator === '*') {
+                pushOperator = keys.concat('*'); 
+                this.props.keyPress(pushOperator);
+            } else if(operator === '-') {
+                pushOperator = keys.concat('-'); 
+                this.props.keyPress(pushOperator);
+            } else if(operator === '+') {
+                pushOperator = keys.concat('+'); 
+                this.props.keyPress(pushOperator);
+            }
+        }
+    }
+
+    multiplyButtonLogic(keys) {
+        if(keys && keys[keys.length - 1] !== '*') {
+            const pushDivideSymbol = keys.concat('*'); 
+            this.props.keyPress(pushDivideSymbol);
         }
     }
 
     render() {
         return(
             <div>
-                {this.ifZeroButton()}
+                {this.buttonType()}
             </div>
         )
     }
--- react_calc/src/components/ButtonPad.js
+++ react_calc/src/components/ButtonPad.js
@@ -10,33 +10,33 @@ class ButtonPad extends React.Component {
                 <table className="buttonsTable">
                     <tbody>
                         <tr>
-                            <td><Button number="C"/></td>
-                            <td><Button number="+/-"/></td>
-                            <td><Button number="%"/></td>
-                            <td><Button number="/"/></td>
+                            <td><Button unit="C"/></td>
+                            <td><Button unit="DEL"/></td>
+                            <td><Button unit="+/-"/></td>
+                            <td><Button unit="/"/></td>
                         </tr>
                         <tr>
-                            <td><Button number="7"/></td>
-                            <td><Button number="8"/></td>
-                            <td><Button number="9"/></td>
-                            <td><Button number="X"/></td>
+                            <td><Button unit="7"/></td>
+                            <td><Button unit="8"/></td>
+                            <td><Button unit="9"/></td>
+                            <td><Button unit="*"/></td>
                         </tr>
                         <tr>
-                            <td><Button number="4"/></td>
-                            <td><Button number="5"/></td>
-                            <td><Button number="6"/></td>
-                            <td><Button number="-"/></td>
+                            <td><Button unit="4"/></td>
+                            <td><Button unit="5"/></td>
+                            <td><Button unit="6"/></td>
+                            <td><Button unit="-"/></td>
                         </tr>
                         <tr>
-                            <td><Button number="1"/></td>
-                            <td><Button number="2"/></td>
-                            <td><Button number="3"/></td>
-                            <td><Button number="+"/></td>
+                            <td><Button unit="1"/></td>
+                            <td><Button unit="2"/></td>
+                            <td><Button unit="3"/></td>
+                            <td><Button unit="+"/></td>
                         </tr>
                         <tr>
-                            <td colSpan="2"><Button number="0"/></td>
-                            <td><Button number="."/></td>
-                            <td><Button number="="/></td>
+                            <td colSpan="2"><Button unit="0"/></td>
+                            <td><Button unit="."/></td>
+                            <td><Button unit="="/></td>
                         </tr>
                     </tbody>
                 </table>
